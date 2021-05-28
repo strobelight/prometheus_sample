90ddeea0a80e2b4774f22439ef001e7b4f582940
Ken Shaffer <8398167+strobelight@users.noreply.github.com>
2021-05-28 07:04:53 -0400

    output location of metrics

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import time
 import sample_counter
 import sample_gauge
 
+port = 8080
+
 if __name__ == "__main__":
     i = pc.Info('info', 'Info on sample app')
     i.info({'version': '1.0', 'maintainer': 'me@email.com'})
     sample_counter.init()
     sample_gauge.init()
-    pc.start_http_server(8080)
+    pc.start_http_server(port)
+    print(f'server started at localhost:{port}/metrics')
     while True:
         time.sleep(1)
